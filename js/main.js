b9f1c9e67118a3047a5ba51d027cfc87a69b8417
Simone Bresci <simone.bresci.05@gmail.com>
2021-02-23 16:01:49 +0100

    file parziali _common.scss e _componentsStyle.scss

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-// Progetto
-// 1) Solo Desktop
-// 2) Header e footer con struttura dati vue
-// Deadline
-// 23/02 ore 18.00
-// 3) Ricorda hover!
-
 var app = new Vue ({
   el: '#root',
   data: {
@@ -15,16 +8,6 @@ var app = new Vue ({
     // FOOTER DATA
     footerLinks: ['FAQ\'s', 'Sitemap', 'Contact Us']
   },
-
-  methods: {
-    // METODI GENERICI *********************************************************
-    // VALORE DA TRASFORMARE CON PROPORZIONE
-    proportion: function(valore1,valore1Max,valore2Max){
-      // Es. 7.5/10 = x / 5
-      // x = (valore1 * valore2Max) / valore1Max
-      return (valore1 * valore2Max) / valore1Max;
-    }
-  }
 
 });
 // TOOLS DI SVILUPPO
